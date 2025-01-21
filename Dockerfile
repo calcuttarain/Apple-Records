e29dc45143a89b64cb9b313e16cd71f2da591bc7
Tudor Pistol <116034514+calcuttarain@users.noreply.github.com>
2025-01-21 19:15:44 +0200

    finally it works

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM composer:lts as deps
 WORKDIR /app
-RUN --mount=type=bind,source=composer.json,target=composer.json \
-    --mount=type=bind,source=composer.lock,target=composer.lock \
-    --mount=type=cache,target=/tmp/cache \
-    composer install --no-dev --no-interaction
+
+COPY composer.json composer.lock ./
+RUN composer install --no-dev --no-interaction
 
 FROM php:8.2-apache as final
 RUN docker-php-ext-install pdo pdo_mysql
--- src/app/core/config.php
+++ src/app/core/config.php
@@ -1,11 +1,23 @@
 <?php
 
-if($_SERVER['SERVER_NAME'] == 'localhost')
-  define('ROOT', 'http://localhost:9000/public');
-else 
-  define('ROOT', 'https://www.example.com');
+define('ROOT_PATH', realpath(dirname(__FILE__) . '/../'));
+require_once ROOT_PATH . '/../vendor/autoload.php';
+
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    define('ROOT', 'http://localhost:9000/public');
+} else {
+    define('ROOT', 'https://www.example.com');
+}
 
 define('APP_NAME', "Apple Records");
 define('APP_DESC', "The Official Record Label of The Beatles");
 
 define('DEBUG', true);
+
+
+define('FROM_EMAIL', getenv('FROM_EMAIL'));
+define('FROM_NAME', getenv('FROM_NAME'));
+define('SMTP_HOST', getenv('SMTP_HOST'));
+define('SMTP_PORT', getenv('SMTP_PORT'));
+define('SMTP_USER', getenv('SMTP_USER'));
+define('SMTP_PASS', getenv('SMTP_PASS'));
