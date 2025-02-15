83d30dc51f64b057e68db8f6f481210621b509f2
Tudor Pistol <116034514+calcuttarain@users.noreply.github.com>
2025-02-15 14:15:03 +0200

    commit

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -27,7 +27,7 @@ trait Database
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch (PDOException $e) {
-            /*die("Database connection failed: " . $e->getMessage());*/
+            die("Database connection failed: " . $e->getMessage());
         }
     }
 
