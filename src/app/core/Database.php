fc8dc94673256c690242222f8f80b87401f52098
Tudor Pistol <116034514+calcuttarain@users.noreply.github.com>
2025-02-15 14:25:07 +0200

    debug

--- src/app/core/Database.php
+++ src/app/core/Database.php
@@ -13,7 +13,7 @@ trait Database
         $this->host = getenv('DB_HOST');
         $this->dbname = getenv('DB_NAME');
         $this->user = getenv('DB_USER');
-        $password = getenv('DB_PASS');
+        $password = 'pass';
 
         $this->connect();
     }
